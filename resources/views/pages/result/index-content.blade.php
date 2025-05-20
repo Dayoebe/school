bbe2a2f72322359bfd2b47a781316b6d37dbf620
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-05-20 01:15:19 +0100

    Update dropdown label from "Change Semester" to "Change Term" for clarity

--- resources/views/pages/result/index-content.blade.php
+++ resources/views/pages/result/index-content.blade.php
@@ -31,11 +31,11 @@
                 </div>
             </div>
 
-            {{-- Change Semester Dropdown --}}
+            {{-- Change Semester (Term) Dropdown --}}
             <div class="relative" x-data="{ open: false }">
                 <button @click="open = !open"
                     class="bg-white border border-blue-500 text-blue-700 px-4 py-2 rounded shadow hover:bg-blue-50 transition">
-                    Change Semester
+                    Change Term
                 </button>
                 <div x-show="open" @click.outside="open = false"
                     class="absolute z-50 mt-2 bg-white border rounded shadow-lg w-64 max-h-60 overflow-auto">
