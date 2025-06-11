cd9cc2be45dbd854333eb123bbab547561066607
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-06-11 03:44:54 +0100

    feat: Add favicon link to print layout for improved branding

--- resources/views/layouts/print.blade.php
+++ resources/views/layouts/print.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('logo.png') }}">
     <title>@yield('title')</title>
     <style>
         *{
