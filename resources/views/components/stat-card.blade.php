1cf053dbc0f5db3d5814026e1cbc4fa686bad4cb
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-06-28 12:35:03 +0100

    Remove unused performer, stat, and summary card components to streamline the codebase

--- resources/views/components/stat-card.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="bg-{{ $color }}-50 p-4 rounded-lg text-center">
-    <p class="text-sm text-{{ $color }}-600 font-semibold">{{ $label }}</p>
-    <p class="text-lg font-bold">{{ $value }}</p>
-</div>
--- resources/views/components/summary-card.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="bg-{{ $color }}-50 rounded-lg p-4 border border-{{ $color }}-200">
-    <div class="flex items-center">
-        <i class="fas fa-{{ $icon }} text-{{ $color }}-600 text-2xl mr-3"></i>
-        <div>
-            <h3 class="text-lg font-semibold text-{{ $color }}-800">{{ $title }}</h3>
-            <p class="text-gray-700">{{ $value }}</p>
-        </div>
-    </div>
-</div>
