9cb9defa712d536ee793111c2e84e8a8dd5918ca
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-07-04 21:01:31 +0100

    Refactor AdminController by removing unnecessary docblocks for improved code clarity and maintainability.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -17,84 +17,48 @@ class AdminController extends Controller
         $this->admin = $admin;
     }
 
-    /**
-     * Display a listing of the resource.
-     */
     public function index(): View
     {
         $this->authorize('viewAny', [User::class, 'admin']);
-
         return view('pages.admin.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create(): View
     {
         $this->authorize('create', [User::class, 'admin']);
-
         return view('pages.admin.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request): RedirectResponse
     {
         $this->authorize('create', [User::class, 'admin']);
         $this->admin->createAdmin($request);
-
         return back()->with('success', 'Admin Created Successfully');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
-     */
     public function show(User $admin): View
     {
         $this->authorize('view', [$admin, 'admin']);
-
         return view('pages.admin.show', compact('admin'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
-     */
     public function edit(User $admin): View
     {
         $this->authorize('update', [$admin, 'admin']);
-
         return view('pages.admin.edit', compact('admin'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
-     */
     public function update(Request $request, User $admin): RedirectResponse
     {
         $this->authorize('update', [$admin, 'admin']);
         $this->admin->updateAdmin($admin, $request->except('_token', '_method'));
-
         return back()->with('success', 'Admin Updated Successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
-     */
     public function destroy(User $admin): RedirectResponse
     {
         $this->authorize('delete', [$admin, 'admin']);
         $this->admin->deleteAdmin($admin);
-
         return back()->with('success', 'Admin Deleted Successfully');
     }
 }
